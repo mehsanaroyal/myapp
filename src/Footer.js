2ed4d062a53b8225c3c3de5078d98d56cd82fd3d
Samir Patel <patelsamir2443@gmail.com>
2021-04-12 20:17:24 +1200

    GH Page commit

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class Footer extends Component
+{
+    render(){
+        return(
+            <footer className ="footer mt-auto py-3 bg-light">
+  <div className ="container">
+    <span className ="text-muted">@Copyright by Samir Patel 30032575</span>
+  </div>
+</footer>
+        );
+    }
+
+}
+export default Footer;
